2af9a6c366a405233c9118e5f83ba6c2e8b4e105
jeff-hykin <jeff.hykin@gmail.com>
2020-02-24 11:54:22 -0600

    - fix help button

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -26,8 +26,8 @@ export default {
                 location: '/'
             },
             {
-                name: 'About',
-                location: '/about'
+                name: 'Help',
+                location: '/help'
             },
         ]
     })
